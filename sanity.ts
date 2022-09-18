e359caa35b2fb0df484b1ccc51578dcdf7a5efbc
gioadorno <adorno.gio@gmail.com>
2022-09-18 00:11:42 -0700

    Update

--- sanity.ts
+++ sanity.ts
@@ -2,10 +2,10 @@ import { createClient } from "next-sanity";
 import createImageUrlBuilder from '@sanity/image-url';
 
 export const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    dataset: 'production',
+    projectId: 'ilueyvg7',
     apiVersion: '2021-03-25',
-    useCdn: process.env.NODE_ENV === 'production'
+    useCdn: true
 };
 
 //  Set up the client for fetching data in the getProps page functions
