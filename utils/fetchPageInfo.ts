973aec8c3aa0e4e233a76497377444149ad2118b
gioadorno <adorno.gio@gmail.com>
2022-09-18 07:54:31 -0700

    Update

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,7 +1,8 @@
 import { PageInfo } from "../typings";
+import { apiURL } from "./url";
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
+    const res = await fetch(`${apiURL}api/getPageInfo`);
 
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
