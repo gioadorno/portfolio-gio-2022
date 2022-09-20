413ae2c1f0e3751ea1dd020ee6a699784abe1b6e
gioadorno <adorno.gio@gmail.com>
2022-09-19 20:59:18 -0700

    Fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -17,7 +17,7 @@ const Skills = ({skills}: Props) => {
     transition={{ duration: 2 }}
     className='flex relative overflow-hidden flex-col md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center '>
         <h3 className='absolute top-20 uppercase tracking-[20px] text-orange-300/70 text-2xl'>Skills</h3>
-        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>Hover over a skill for curret proficiency</h3>
+        <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm '>Hover over a skill for current proficiency</h3>
 
         <div className='grid grid-cols-8 gap-5 pt-24'>
           {skills?.slice(0, skills.length / 2).map((skill) => (
